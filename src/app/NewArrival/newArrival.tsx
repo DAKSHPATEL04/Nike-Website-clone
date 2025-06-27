ba894034b12f5f607db5553614ee56e372f871e3
Daksh <pateldaksh382@gmail.com>
2025-06-27 14:07:55 +0530

    fix  newarrival component

--- src/app/NewArrival/newArrival.tsx
+++ src/app/NewArrival/newArrival.tsx
@@ -1,55 +1,46 @@
-const newArrival = () => {
+const NewArrival = () => {
   return (
     <div className="flex bg-white justify-center items-center w-full py-10">
-      <div className="flex flex-col justify-center w-full  items-center">
-        {" "}
-        <div className="w-full flex justify-start  px-[59px]">
-          {" "}
-          <h1
-            className="  text-black"
-            style={{
-              fontSize: "25px",
-              fontWeight: "500",
-            }}
-          >
+      <div className="flex flex-col justify-center w-full items-center">
+        {/* Heading */}
+        <div className="w-full flex justify-start px-[59px]">
+          <h1 className="text-black text-[25px] font-medium">
             Don&apos;t Miss
           </h1>
         </div>
-        <div
-          className="flex flex-col w-[1400px]  text-black gap-4 justify-center items-center"
-          style={{}}
-        >
-          <div className=" text-center ">
-            <img src="/img/new.png" alt="" />
-            <div>
-              <h3
-                className="pt-9
-              "
-                style={{
-                  fontWeight: "500",
-                }}
-              >
-                Women&apos;s Air Jordan Collection
-              </h3>
-              <h1
-                className=""
-                style={{
-                  fontFamily: "roboto",
-                  fontWeight: "900",
-                  fontSize: "60px",
-                }}
-              >
-                SHOW &apos;EM UP
-              </h1>
-              <p style={{ fontFamily: "poppins" }}>
-                Crafted for your flyest self, the new Air Jordan Collection
-                brings iconic prints and elevated cuts.
-              </p>
-              <div className=" py-4">
-                <button className="bg-black text-white py-2 px-4 rounded-3xl">
-                  Shop
-                </button>
-              </div>
+
+        {/* Main Content */}
+        <div className="flex flex-col w-[1400px] text-black gap-6 justify-center items-center">
+          <div className="text-center">
+            <img src="/img/new.png" alt="New Arrival" />
+
+            {/* Subheading */}
+            <h3 className="pt-9 font-medium text-lg">
+              Women&apos;s Air Jordan Collection
+            </h3>
+
+            {/* Main Heading with Custom Font */}
+            <h1
+              style={{
+                fontWeight: "900",
+                fontSize: "60px",
+                margin: "20px 0",
+              }}
+            >
+              SHOW &apos;EM UP
+            </h1>
+
+            {/* Description */}
+            <p className="font-[poppins] text-base max-w-[600px] mx-auto">
+              Crafted for your flyest self, the new Air Jordan Collection brings
+              iconic prints and elevated cuts.
+            </p>
+
+            {/* CTA Button */}
+            <div className="py-5">
+              <button className="bg-black text-white py-2 px-6 rounded-3xl text-sm font-medium">
+                Shop
+              </button>
             </div>
           </div>
         </div>
@@ -58,4 +49,4 @@ const newArrival = () => {
   );
 };
 
-export default newArrival;
+export default NewArrival;
